f0302ef2a3da442c1e12df126dbbeb35fb57a9a2
bredele <olivier.wietrich@gmail.com>
2014-07-20 16:56:46 -0600

    add documentation

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ module.exports = Stack;
 
 /**
  * Stack constructor.
+ *
+ * A stack is document fragment attached
+ * to a root node (el).
+ *
+ * Examples:
+ *
+ *   var stack = new Stack(el);
+ *
+ * @param {Element} el
  * @api public
  */
 
@@ -18,20 +27,68 @@ function Stack(el) {
   this.nodes = {};
 }
 
+
+/**
+ * Append child.
+ * 
+ * @param  {Element} parent
+ * @param  {Element} child
+ * @return {Boolean} true if appenned
+ * @api private
+ */
+
 function append(parent, child) {
   if(child) return !!parent.appendChild(child);
   return false;
 }
 
+
+/**
+ * Get stacked node.
+ *
+ * @param  {String} name
+ * @return {Element}
+ * @api public
+ */
+
 Stack.prototype.get = function(name) {
   return this.nodes[name];
 };
+
+
+/**
+ * Add node element into the stack.
+ *
+ * Examples
+ *
+ *   stack.add('beep', el);
+ *   
+ * @param {String} name
+ * @param {Element} node
+ * @api public
+ */
 
 Stack.prototype.add = function(name, node) {
   this.nodes[name] = node;
   append(this.fragment, node);
   return this;
 };
+
+
+/**
+ * Show stacked node into the stack root.
+ *
+ * To be shown a node has to be first added in
+ * the stack.
+ *
+ * Examples
+ *
+ *   stack.show('beep');
+ *   
+ * @param  {String} name
+ * @return {this}
+ * @api public
+ */
 
 Stack.prototype.show = function(name) {
   if(!append(this.el, this.get(name))) return;
