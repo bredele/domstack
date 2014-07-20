f13ddc58e540d63cb078e56e0ba21a7deda303ce
bredele <olivier.wietrich@gmail.com>
2014-07-20 16:06:50 -0600

    do testeyyyy

---
+++ test/stack.js
@@ -0,0 +1,19 @@
+/**
+ * Test dependencies.
+ */
+
+var assert = require('assert');
+var Stack = require('domstack');
+
+describe("add", function() {
+
+	var stack, parent;
+	beforeEach(function() {
+		parent = document.createElement('div');
+		stack = new Stack(parent);
+	});
+
+
+	
+});
+
